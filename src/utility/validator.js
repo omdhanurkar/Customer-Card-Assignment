26d8ee95e08d5d782ef81dfae59637365f0d54fb
Omprakash <omdhanurkar444@gmail.com>
2023-02-04 00:09:31 +0530

    validations

--- src/utility/validator.js
+++ src/utility/validator.js
@@ -22,7 +22,7 @@ const isVAlidEmail = function (email) {
     return (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     ).test(email)
 }
-
+ 
 
 const isValidPhone = function (phone) {
     return (/^[6-9]\d{9}$/).test(phone)
